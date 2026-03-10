508afd0cccebb219c0e58ecc1f9eb2333a6f0adf
Gaozhongpai <gaozhongpai@gmail.com>
2026-03-10 16:55:54 -0400

    Initial project page for MedGRPO (CVPR 2026)

---
+++ static/js/main.js
@@ -0,0 +1,13 @@
+// Copy citation to clipboard
+function copyCitation() {
+  const text = document.getElementById('citation-text').innerText;
+  navigator.clipboard.writeText(text).then(() => {
+    const btn = document.querySelector('.copy-btn');
+    btn.innerHTML = '<i class="fas fa-check"></i> Copied!';
+    btn.classList.add('copied');
+    setTimeout(() => {
+      btn.innerHTML = '<i class="fas fa-copy"></i> Copy';
+      btn.classList.remove('copied');
+    }, 2000);
+  });
+}
